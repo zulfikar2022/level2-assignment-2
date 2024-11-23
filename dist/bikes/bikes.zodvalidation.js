29df376acb0a5b857549142146bd594b09b2c708
zulfikar2022 <sayedzulfikar2019@gmail.com>
2024-11-23 08:15:31 +0600

    refactor: improve validation schemas by enforcing strict mode and enhancing readability

--- dist/bikes/bikes.zodvalidation.js
+++ dist/bikes/bikes.zodvalidation.js
@@ -1,18 +1,25 @@
 import { z } from "zod";
 import { BikeCategory } from "./bikes.interfaces.js";
-const bikeValidationSchema = z.object({
+const bikeValidationSchema = z
+    .object({
     name: z.string().min(1, { message: "Name is required" }),
     brand: z.string().min(1, { message: "Brand is required" }),
     price: z.number().positive({ message: "Price must be a positive number" }),
     category: z.nativeEnum(BikeCategory, { message: "Invalid bike category" }),
-    quantity: z.number().int().min(0, { message: "Quantity cannot be negative" }),
+    quantity: z
+        .number()
+        .int()
+        .min(0, { message: "Quantity cannot be negative" }),
     description: z.string().optional(),
-});
-const orderValidationSchema = z.object({
+})
+    .strict();
+const orderValidationSchema = z
+    .object({
     email: z.string().email({ message: "Invalid email address" }),
     product: z.string({ message: "Invalid product ID" }),
     quantity: z.number().int({ message: "Quantity must be a number" }).min(1, {
         message: "Quantity must be a positive integer and greater than 0",
     }),
-});
+})
+    .strict();
 export { bikeValidationSchema, orderValidationSchema };
